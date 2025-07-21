a1b08aea3a8a52ece65005e9ede513651d2738d5
codewithsaidul <codewithsaidul@gmail.com>
2025-07-21 14:07:15 +0600

    part-6 get & update status booking api added

--- server/src/app/Modules/booking/bookin.controller.ts
+++ server/src/app/Modules/booking/bookin.controller.ts
@@ -5,70 +5,74 @@ import { sendResponse } from "../../utils/sendResponse";
 import { BookingServices } from "./booking.service";
 import { JwtPayload } from "jsonwebtoken";
 
-
-
-
-const createBooking = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
-    const decodedToken = req.user as JwtPayload
-    const booking = await BookingServices.createBooking(req.body, decodedToken._id)
-      sendResponse(res, {
-        statusCode: 201,
-        success: true,
-        message: "Booking created successfully",
-        data: booking,
+const createBooking = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const decodedToken = req.user as JwtPayload;
+    const booking = await BookingServices.createBooking(
+      req.body,
+      decodedToken._id
+    );
+    sendResponse(res, {
+      statusCode: 201,
+      success: true,
+      message: "Booking created successfully",
+      data: booking,
     });
-});
+  }
+);
 
-
-
-const getUserBookings = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
-    const booking = await BookingServices.getUserBookings()
-      sendResponse(res, {
-        statusCode: 200,
-        success: true,
-        message: "All User Bookings retrive successfully",
-        data: booking,
+const getUserBookings = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const decodedToken = req.user as JwtPayload;
+    const query = req.query;
+    const booking = await BookingServices.getUserBookings(query as Record<string, string>, decodedToken._id);
+    sendResponse(res, {
+      statusCode: 200,
+      success: true,
+      message: "User Bookings retrive successfully",
+      data: booking,
     });
-});
+  }
+);
 
-
-
-const getAllBookings = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
-    const booking = await BookingServices.getAllBookings()
-      sendResponse(res, {
-        statusCode: 200,
-        success: true,
-        message: "All Bookings retrive successfully",
-        data: booking,
+const getAllBookings = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const query = req.query;
+    const booking = await BookingServices.getAllBookings(query as Record<string, string>);
+    sendResponse(res, {
+      statusCode: 200,
+      success: true,
+      message: "All Bookings retrive successfully",
+      data: booking,
     });
-});
+  }
+);
 
-
-const getBookingById = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
-    const booking = await BookingServices.getBookingById()
-      sendResponse(res, {
-        statusCode: 200,
-        success: true,
-        message: "Booking retrive successfully",
-        data: booking,
+const getBookingById = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const bookingId = req.params.bookingId
+    const booking = await BookingServices.getBookingById(bookingId);
+    sendResponse(res, {
+      statusCode: 200,
+      success: true,
+      message: "Booking retrive successfully",
+      data: booking,
     });
-});
+  }
+);
 
-
-const updateBookingStatus = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
-    const booking = await BookingServices.updateBookingStatus()
-      sendResponse(res, {
-        statusCode: 200,
-        success: true,
-        message: "Booking status has been updated successfully",
-        data: booking,
+const updateBookingStatus = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const bookingId = req.params.bookingId;
+    const booking = await BookingServices.updateBookingStatus(bookingId, req.body);
+    sendResponse(res, {
+      statusCode: 200,
+      success: true,
+      message: "Booking status has been updated successfully",
+      data: booking,
     });
-})
-
-
-
-
-
+  }
+);
 
 export const BookingController = {
   createBooking,
--- server/src/app/Modules/booking/booking.service.ts
+++ server/src/app/Modules/booking/booking.service.ts
@@ -1,5 +1,6 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { AppError } from "../../errorHelpers/AppError";
+import { QueryBuilder } from "../../utils/queryBuilder";
 import { PaymentStatus } from "../payment/payment.interface";
 import { Payment } from "../payment/payment.model";
 import { ISSLCommerz } from "../sslCommerz/sslCommerz.interface";
@@ -36,18 +37,28 @@ const createBooking = async (payload: Partial<IBooking>, userId: string) => {
     // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
     const amount = Number(tour.costFrom) * Number(payload.guestCount!);
 
-    const booking = await Booking.create([{
-      user: userId,
-      status: BookingStatus.PENDING,
-      ...payload,
-    }], { session });
+    const booking = await Booking.create(
+      [
+        {
+          user: userId,
+          status: BookingStatus.PENDING,
+          ...payload,
+        },
+      ],
+      { session }
+    );
 
-    const payment = await Payment.create([{
-      booking: booking[0]._id,
-      transactionId: transactionId,
-      amount: amount,
-      status: PaymentStatus.UNPAID,
-    }], { session });
+    const payment = await Payment.create(
+      [
+        {
+          booking: booking[0]._id,
+          transactionId: transactionId,
+          amount: amount,
+          status: PaymentStatus.UNPAID,
+        },
+      ],
+      { session }
+    );
 
     const updatedBooking = await Booking.findByIdAndUpdate(
       booking[0]._id,
@@ -58,57 +69,120 @@ const createBooking = async (payload: Partial<IBooking>, userId: string) => {
       .populate("tour", "title costFrom")
       .populate("payment");
 
+    const userName = (updatedBooking?.user as any).name;
+    const userEmail = (updatedBooking?.user as any).email;
+    const userPhone = (updatedBooking?.user as any).phone;
+    const userAddress = (updatedBooking?.user as any).address;
 
+    const sslPayload: ISSLCommerz = {
+      name: userName,
+      email: userEmail,
+      phoneNumber: userPhone,
+      address: userAddress,
+      amount: amount,
+      transactionId: transactionId,
+    };
 
-      const userName = (updatedBooking?.user as any).name;
-      const userEmail = (updatedBooking?.user as any).email;
-      const userPhone = (updatedBooking?.user as any).phone;
-      const userAddress = (updatedBooking?.user as any).address;
-
-
-      const sslPayload: ISSLCommerz = {
-        name: userName,
-        email: userEmail,
-        phoneNumber: userPhone,
-        address: userAddress,
-        amount: amount,
-        transactionId: transactionId
-      }
-
-
-
-      const sslPayment = await SSLServices.sslPaymentInit(sslPayload);
+    const sslPayment = await SSLServices.sslPaymentInit(sslPayload);
 
     // end session
-      await session.commitTransaction();
-      await session.endSession();
-
+    await session.commitTransaction();
+    await session.endSession();
 
     return {
       paymentUrl: sslPayment.GatewayPageURL,
-      booking: updatedBooking
+      booking: updatedBooking,
     };
   } catch (error) {
     // end session
     await session.abortTransaction();
     await session.endSession();
-    throw error
+    throw error;
   }
 };
 
-const getUserBookings = async () => {
-  return {};
+const getUserBookings = async (
+  query: Record<string, string>,
+  userId: string
+) => {
+  const myBookings = await Booking.find({ user: userId });
+
+  if (myBookings.length <= 0) {
+    throw new AppError(
+      404,
+      "You haven't made any booking yet. Please Book a tour first"
+    );
+  }
+
+  const queryBuilder = new QueryBuilder(Booking.find({ user: userId }), query);
+
+  const bookings = queryBuilder.filter().sort().fields().paginate();
+
+  // const meta = await queryBuilder.getMeta();
+
+  const [data, meta] = await Promise.all([
+    bookings.build(),
+    queryBuilder.getMeta(),
+  ]);
+
+  return {
+    data,
+    meta,
+  };
 };
 
-const getBookingById = async () => {
-  return {};
+const getBookingById = async (bookingId: string) => {
+  const booking = await Booking.findById(bookingId);
+  return {
+    data: booking,
+  };
 };
 
-const getAllBookings = async () => {
-  return {};
+const getAllBookings = async (query: Record<string, string>) => {
+  const queryBuilder = new QueryBuilder(Booking.find(), query);
+
+  const bookings = queryBuilder.filter().sort().fields().paginate();
+
+  // const meta = await queryBuilder.getMeta();
+
+  const [data, meta] = await Promise.all([
+    bookings.build(),
+    queryBuilder.getMeta(),
+  ]);
+
+  return {
+    data,
+    meta,
+  };
 };
 
-const updateBookingStatus = async () => {
+const updateBookingStatus = async (bookingId: string, payload: Partial<IBooking>) => {
+  const session = await Booking.startSession();
+
+  try {
+    session.startTransaction();
+
+    const updatedBookingData  = {
+      ...payload,
+      status: BookingStatus.CANCELLED
+    }
+
+    const updatedBooking = await Booking.findByIdAndUpdate(bookingId, updatedBookingData, { new: true, runValidators: true, session } );
+
+    await Payment.findByIdAndUpdate(updatedBooking?.payment, { status: PaymentStatus.CANCELLED }, { runValidators: true, session })
+
+    await session.commitTransaction()
+    await session.endSession()
+
+    return {
+      data: updatedBooking
+    };
+  } catch (error) {
+    session.abortTransaction();
+    session.endSession()
+    throw error
+  }
+
   return {};
 };
 
